82608b0616ea26fb4368bffbe4740d115751da9e
joshua Odermatt <joshi.odermatt@gmail.com>
2019-12-18 16:01:56 +0100

    las push of the day

--- index.php
+++ index.php
@@ -1,15 +1,32 @@
-<!DOCTYPE html>
 <html lang="de">
   <head>
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Titel</title>
-    <style>
-
-    </style>
+    <link rel="stylesheet" href="../css/index.css">
+    <title>UB</title>
   </head>
   <body>
-    <p>heheheh</p>
-    <button class="learn-more">Learn More</button>
+    <header class="header">
+      <div id="logo-container"> 
+        <img src="../recources/logo.png" alt="snake logo" id="logo">
+      </div>
+      <div id="nav">
+        <p><a href="#" class="nav-items">HOME</a></p>
+        <p><a href="#" class="nav-items">GLOBAL RANKING</a></p>
+        <p><a href="#" class="nav-items">GLOBAL STATS</a></p>
+      </div>
+      <div id="header-space">
+      </div>
+      <div class="login-elements">
+        <p>username<p>
+        <p>logout</p>
+        <p>login</p>
+      </div>
+    </header>
   </body>
 </html>
+
+
+
+    
